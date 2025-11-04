2600f50bba482c06cd46ad72e5670d4652baa49e
yenkj <ykj363963169@gmail.com>
2025-11-04 16:03:45 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   ignorePatterns: ['src/types/**/*.d.ts'],  
   rules: {
     'no-unused-vars': 'off',
-    'no-console': 'warn',
+    'no-console': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react/no-unescaped-entities': 'off',
     //添加这三行
